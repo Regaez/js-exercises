98e273dfc79498fab2ac91e181e1cb1b09c66593
Trang Hong Thi Nguyen <trang.nguyenthihong@live.de>
2017-02-03 12:47:54 +0100

    exercise 1 done

--- 1-counter/app.js
+++ 1-counter/app.js
@@ -2,5 +2,18 @@
 	'use-strict';
 
 	// ENTER YOUR CODE HERE
-var x;
+
+	var decr = document.getElementsByClassName('decrement'); //is giving me a list (HTMLCollection)
+	var incr = document.getElementsByClassName('increment');
+
+	decr[0].addEventListener('click', function(){
+	 	var newOutput = document.getElementsByClassName('output');
+		//console.log(newOutput[0].innerHTML--);
+		newOutput[0].innerHTML--; // .innerHTML only for single elements - no HTMLCollection
+	});
+
+	incr[0].addEventListener('click', function(){
+		var newOutput =  document.getElementsByClassName('output');
+		newOutput[0].innerHTML++;
+	});
 })();
