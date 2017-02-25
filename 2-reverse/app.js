fc8697fe7dbda906ccc9ecf61cfb8f6e23e4826b
Trang Hong Thi Nguyen <trang.nguyenthihong@live.de>
2017-02-25 15:29:14 +0100

    defining loop for reversing the string

--- 2-reverse/app.js
+++ 2-reverse/app.js
@@ -3,4 +3,17 @@
 
 	// ENTER YOUR CODE HERE
 
+ 	var str = document.querySelector('input').value;
+
+	//console.log(str);
+	var newStr = '';
+
+	for(var i = str.size-1; i >= 0; i--){
+		newStr += str[i];
+		console.log(newStr);
+	}
+
+	document.querySelector('output').value = newStr;
+
+
 })();
