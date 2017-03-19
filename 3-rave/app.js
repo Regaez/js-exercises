4ab9d05fe77846d728f8a5a772dd64673c382952
Trang Hong Thi Nguyen <trang.nguyenthihong@live.de>
2017-03-19 17:36:43 +0100

    excercise 3 done

--- 3-rave/app.js
+++ 3-rave/app.js
@@ -3,4 +3,15 @@
 
 	// ENTER YOUR CODE HERE
 
+	var colors = ['#6495ED', '#F08080', '#66CDAA', '#DDA0DD'];
+
+	/*body.addEventListener('load', setIntervall(){
+			function(){
+				body.style.backgroundColor = colors[Math.floor(colors.length * Math.random())];
+			}, 500);
+	});*/
+
+	var x = setInterval(function(){
+		document.body.style.backgroundColor = colors[Math.floor(colors.length * Math.random())];
+	}, 500);
 })();
